8913a4bcd56f71beb9b1e277b8a24276f90f4f7b
Nicolas Joyard <joyard.nicolas@gmail.com>
2016-08-27 17:27:08 +0200

    Aggregation separee avant/apres municipales 2014

--- bin/aggregate-presences.py
+++ bin/aggregate-presences.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+breaks = {
+    '2012-03-02': '2012',
+    '2014-04-30': '2014'
+}
+
 if __name__ == '__main__':
-    bydate = []
-    byname = {}
+    for k, v in breaks.iteritems():
+        breaks[k] = {'nom': v, 'byname': {}, 'bydate': []}
 
     with open('data/presences-cm-nettoye.csv', 'r') as infile:
         lastdate = ''
+        curbreak = None
         curitem_bydate = None
 
         for item in [line.split(';') for line in infile.read().decode('utf-8').split('\n')[1:]]:
@@ -17,36 +23,41 @@ if __name__ == '__main__':
             nom = item[1]
             present = item[2]
 
-            if nom not in byname:
-                byname[nom] = {'presences': 0, 'absences': 0}
+            if date in breaks:
+                curbreak = date
+
+            if nom not in breaks[curbreak]['byname']:
+                breaks[curbreak]['byname'][nom] = {'presences': 0, 'absences': 0}
 
             if date != lastdate:
                 if curitem_bydate:
-                    bydate.append(curitem_bydate)
+                    breaks[curbreak]['bydate'].append(curitem_bydate)
 
                 curitem_bydate = {'date': date, 'presents': 0, 'absents': 0}
 
             if present == '1':
                 curitem_bydate['presents'] = curitem_bydate['presents'] + 1
-                byname[nom]['presences'] = byname[nom]['presences'] + 1
+                breaks[curbreak]['byname'][nom]['presences'] = breaks[curbreak]['byname'][nom]['presences'] + 1
             else:
                 curitem_bydate['absents'] = curitem_bydate['absents'] + 1
-                byname[nom]['absences'] = byname[nom]['absences'] + 1
+                breaks[curbreak]['byname'][nom]['absences'] = breaks[curbreak]['byname'][nom]['absences'] + 1
 
             lastdate = date
 
         if curitem_bydate:
-            bydate.append(curitem_bydate)
+            breaks[curbreak]['bydate'].append(curitem_bydate)
 
-    with open('data/presences-cm-count-by-date.csv', 'w') as outfile:
-        outfile.write('date;presents;absents\n')
+    for k, v in breaks.iteritems():
 
-        for item in bydate:
-            outfile.write('%s;%s;%s\n' % (item['date'], item['presents'], item['absents']))
+        with open('data/presences-cm-%s-count-by-date.csv' % v['nom'], 'w') as outfile:
+            outfile.write('date;presents;absents\n')
 
-    with open('data/presences-cm-count-by-name.csv', 'w') as outfile:
-        outfile.write('nom;presences;absences\n')
+            for item in v['bydate']:
+                outfile.write('%s;%s;%s\n' % (item['date'], item['presents'], item['absents']))
 
-        for nom, valeurs in byname.iteritems():
-            ligne = '%s;%s;%s\n' % (nom, valeurs['presences'], valeurs['absences'])
-            outfile.write(ligne.encode('utf-8'))
+        with open('data/presences-cm-%s-count-by-name.csv' % v['nom'], 'w') as outfile:
+            outfile.write('nom;presences;absences\n')
+
+            for nom, valeurs in v['byname'].iteritems():
+                ligne = '%s;%s;%s\n' % (nom, valeurs['presences'], valeurs['absences'])
+                outfile.write(ligne.encode('utf-8'))
